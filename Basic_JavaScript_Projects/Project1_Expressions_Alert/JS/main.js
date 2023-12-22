59316cba566b6209e3a952d50aec879d55b51be0
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2023-12-22 12:32:39 -0700

    Add files via upload

---
+++ Basic_JavaScript_Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,15 @@
+window.alert("Hello,World");//popping out windows alert
+document.write("Hello, World!");//writes Hello World on the screen
+var A = "This is a string";//assigning a variable value
+document.write(A);
+window.alert("A");
+document.write("The World says, \"Hello!\"");
+//Creating 2 variables
+var Sent1 = "This is the beginning of the String";
+var Sent2 = "This is the end of the String";
+document.write(Sent1 + Sent2);
+//Another examples of variables
+var car = "The Racers", Blue = "Lambo", Red = "Ferrari", White = "Bugatti";
+document.write(Blue);
+//expression
+document.write(4 + 4);
