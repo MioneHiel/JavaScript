d916998b1a3703743fe6a4d2cc1509d7ce08ba80
Aga Palaganas <149645611+MioneHiel@users.noreply.github.com>
2023-12-22 18:12:28 -0700

    Add files via upload

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function myFunction()
+{
+    //creating the variable
+    var sentence = "I am learning";
+    //using the += operator to add to the variable
+    sentence += " a lot from this book";
+    //using document.getElementById
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
